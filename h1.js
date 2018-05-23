df1e9f94724643798d7ea0961a91e6d3f27ea09e
jaguar3096 <jaguar3096@gmail.com>
2018-05-23 09:34:29 +0800

    change

--- h1.js
+++
@@ -1,7 +0,0 @@
-﻿document.write("<ul>") 
-document.write("<li><p><a href='http://maev.nsysu.edu.tw/files/14-1114-183988,r158-1.php?Lang=zh-tw'>4/4~4/8由張揚祺主任、李宗霖特聘教授及陳邦富教授代表本系至大連理工大學海洋科學與技術學院進行學術交流</a></p></li>")  
-document.write("<li><p><a href='http://maev.nsysu.edu.tw/files/14-1114-183961,r158-1.php?Lang=zh-tw'>賀!海工系2018大環盃獲得2項球類亞軍</a></p></li>")  
-document.write("<li><p><a href='http://maev.nsysu.edu.tw/files/14-1114-183111,r158-1.php?Lang=zh-tw'>106學年度第1學期本系「教學優良課程」名單</a></p></li>")  
-document.write("<li><p><a href='http://maev.nsysu.edu.tw/files/14-1114-181633,r158-1.php?Lang=zh-tw'>恭賀本系大四林威宇錄取日本交換生</a></p></li>")  
-document.write("</ul>") 
-document.write("<a style = 'color:gray';>更新時間 : 2018-04-14 02:07</a>")
